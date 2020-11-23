ea1289e0ed75bd830a731979eec84f282a51237b
mmartine <mmartinez_2176@mac.com>
2020-11-23 15:01:53 -0800

    Initial commit

---
+++ src/com/gds/codingProblems/Main.java
@@ -0,0 +1,11 @@
+package com.gds.codingProblems;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("thest");
+        System.out.println("The next stuff");
+
+    }
+}
